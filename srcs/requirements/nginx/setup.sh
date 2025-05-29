541accf9a4accfbfec6851a7bed0003691375777
Maxime Juncker <maxime.junck@gmail.com>
2025-05-29 16:43:30 +0200

    added mariadb and wordpress

--- srcs/requirements/nginx/setup.sh
+++ srcs/requirements/nginx/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-CERT_DIR="./"
-KEY_FILE="$CERT_DIR/server.key"
-CERT_FILE="$CERT_DIR/server.crt"
+CERT_DIR="/etc/nginx/ssl"
+KEY_FILE="$CERT_DIR/nginx.key"
+CERT_FILE="$CERT_DIR/nginx.crt"
 SUBJ="/C=FR/ST=ge/L=CHARBONIERE/O=ME/CN=test.com"
 
-# mkdir -p "$CERT_DIR"
+mkdir "$CERT_DIR"
 
 openssl req -x509 -nodes -newkey rsa:2048 \
-    -keyout "$KEY_FILE" \
-    -out "$CERT_FILE" \
-    -days 365 \
-    -subj "$SUBJ"
+	-keyout "$KEY_FILE" \
+	-out "$CERT_FILE" \
+	-days 365 \
+	-subj "$SUBJ"
