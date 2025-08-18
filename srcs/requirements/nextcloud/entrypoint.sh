d35a10e7e4292e51a273c3596e07375517629ea4
Maxime Juncker <maxime.junck@gmail.com>
2025-08-18 15:06:07 +0200

    fix asset not loading in nextcloud

--- srcs/requirements/nextcloud/entrypoint.sh
+++ srcs/requirements/nextcloud/entrypoint.sh
@@ -3,15 +3,9 @@
 cd /var/www/
 chown -R www-data:root /var/www;
 
-# cp config.sample.php config.php
-
 if [ "$(ls -A /var/www/nextcloud)" ]; then
 	echo nextcloud is already installed!
-exec php-fpm83 --nodaemonize
-
-# php -S 0.0.0.0:4242
-# exec tail -f
-
+	exec php-fpm83 --nodaemonize
 fi
 
 mv /www.conf /etc/php83/php-fpm.d/www.conf
@@ -33,6 +27,7 @@ find /var/www/nextcloud -type d -exec chmod 755 {} \;
 chmod +x /var/www/nextcloud/occ
 
 echo installing nextcloud
+mkdir -p /var/www/nextcloud/nextcloud
 php -d memory_limit=512M occ maintenance:install \
   --database="mysql" \
   --database-name="nextcloud" \
@@ -42,29 +37,11 @@ php -d memory_limit=512M occ maintenance:install \
   --database-port="3306" \
   --admin-user="mjuncker" \
   --admin-pass="mjuncker" \
-  --data-dir="/var/www/nextcloud/data"
+  --data-dir="/var/www/nextcloud/nextcloud/data"
 
 chmod 644 /var/www/nextcloud/config/config.php 
-
-# curl -o nextcloud.zip https://download.nextcloud.com/server/releases/latest.zip
-# unzip nextcloud.zip # will create nextcloud folder
-# chmod 777 -R /var/www/nextcloud
-
-# cd /var/www/nextcloud
-
-# # php -d memory_limit=1024M occ maintenance:install --database="mysql" --database-name="nextcloud" --database-host="mariadb" --database-port="3306" --database-user="mysql" --database-pass="xx_mysql_Passw09_xx" --admin-user="admin" --admin-pass="password"
-# # chmod 777 /var/www/nextcloud/config/config.php
-
-# cp config.sample.php config.php
-
-
-
 chown -R www-data:www-data /var/www/nextcloud
 
 ls -l /var/www/nextcloud/
 
 exec php-fpm83 --nodaemonize
-
-
-# php -S 0.0.0.0:4242
-# exec tail -f
--- srcs/requirements/nginx/setup.sh
+++ srcs/requirements/nginx/setup.sh
@@ -5,15 +5,18 @@ KEY_FILE="$CERT_DIR/nginx.key"
 CERT_FILE="$CERT_DIR/nginx.crt"
 SUBJ="/C=FR/ST=ge/L=CHARBONIERE/O=ME/CN=mjuncker.42.fr"
 
+mkdir -p "$CERT_DIR"
+
 if [ "$(ls -A $CERT_DIR)" ]; then
-	echo cert key are already generated!
-	exit 0
+	echo "key already generated"
+	exec nginx -g "daemon off;"
 fi
 
-mkdir "$CERT_DIR"
-
+echo "generating new ssl keys"
 openssl req -x509 -nodes -newkey rsa:2048 \
 	-keyout "$KEY_FILE" \
 	-out "$CERT_FILE" \
 	-days 365 \
 	-subj "$SUBJ"
+
+exec nginx -g "daemon off;"
