b9a7f32ca67f7149243c6ba91eedcad3bc3f0862
Maxime Juncker <maxime.junck@gmail.com>
2025-08-18 12:40:21 +0200

    nextcloud and wordpress sharing nginx

--- srcs/requirements/nginx/setup.sh
+++ srcs/requirements/nginx/setup.sh
@@ -5,6 +5,11 @@ KEY_FILE="$CERT_DIR/nginx.key"
 CERT_FILE="$CERT_DIR/nginx.crt"
 SUBJ="/C=FR/ST=ge/L=CHARBONIERE/O=ME/CN=mjuncker.42.fr"
 
+if [ "$(ls -A $CERT_DIR)" ]; then
+	echo cert key are already generated!
+	exit 0
+fi
+
 mkdir "$CERT_DIR"
 
 openssl req -x509 -nodes -newkey rsa:2048 \
