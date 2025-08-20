38e0a6219c7ec323df853decbe2589459e313aa8
Maxime Juncker <maxime.junck@gmail.com>
2025-08-20 15:49:21 +0200

    remove print

--- new-cat.py
+++ new-cat.py
@@ -2,7 +2,6 @@ import requests
 import json
 import time
 import signal
-import sys
 
 running = True
 
@@ -18,12 +17,8 @@ while running:
 	if (response.status_code != 200):
 		raise Exception("request failed")
 	data = response.json()
-	print(json.dumps(response.json(), indent=4))
-	print(data)
 	url = data[0]["url"]
 	response = requests.get(url)
 	with open('img.png', 'wb') as f:
 		f.write(response.content)
 	time.sleep(2)
-
-sys.exit(0)
