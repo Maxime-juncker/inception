3ac21eb5f1869424be6bd7a0d4e987ad40258785
Maxime Juncker <maxime.junck@gmail.com>
2025-08-20 15:27:53 +0200

    fix sig

--- new-cat.py
+++ new-cat.py
@@ -1,8 +1,19 @@
 import requests
 import json
 import time
+import signal
+import sys
 
-while True:
+running = True
+
+def signal_handler(sig, frame):
+	global running
+	running = False
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+while running:
 	response = requests.get("https://api.thecatapi.com/v1/images/search")
 	if (response.status_code != 200):
 		raise Exception("request failed")
@@ -14,3 +25,5 @@ while True:
 	with open('img.png', 'wb') as f:
 		f.write(response.content)
 	time.sleep(2)
+
+sys.exit(0)
